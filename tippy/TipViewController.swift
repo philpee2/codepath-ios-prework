abccab1a1b193d24ad68b4c4088337299b576552
Phil Nachum <phil.nachum@airbnb.com>
2016-07-16 14:49:11 -0700

    Rename ViewController to TipViewController

--- tippy/TipViewController.swift
+++ tippy/TipViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
 
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
